fd6d8c7869f849a2e1d0311340d4842e59bd3dc6
Cristian <brito.cristian@gmail.com>
2020-11-24 21:26:34 -0300

    Updates

---
+++ precache-manifest.eadd5b008ec5bbd9f410536836d6ec3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48df6d2ae5b64c8a3a8481fd9f4fc669",
+    "url": "/Prueba/index.html"
+  },
+  {
+    "revision": "a5c06ca401396c9ccbb2",
+    "url": "/Prueba/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "a9c0fb4ec905cb706704",
+    "url": "/Prueba/static/css/main.6cc701a1.chunk.css"
+  },
+  {
+    "revision": "a5c06ca401396c9ccbb2",
+    "url": "/Prueba/static/js/2.f4e023b3.chunk.js"
+  },
+  {
+    "revision": "a9c0fb4ec905cb706704",
+    "url": "/Prueba/static/js/main.cf421a4e.chunk.js"
+  },
+  {
+    "revision": "58dca6bc67eb5ee8b2d3",
+    "url": "/Prueba/static/js/runtime-main.47c76a90.js"
+  }
+]);
